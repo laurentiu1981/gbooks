77fe5296f078e7669e7b7b067ec3914362233ad4
Dia <dianapetrus13081997@gmail.com>
2018-09-14 04:14:07 -0700

    GBOOK-7: Apply bootstrap theme classes for top menu

--- views/layouts/partials/header.tpl.php
+++ views/layouts/partials/header.tpl.php
@@ -1,6 +1,14 @@
-<ul class="menu">
-    <li><a href="/"><img src="/images/books_logo.png"/></a></li>
-    <li><a href="/">Home</a></li>
-    <li><a href="/admin">Admin</a></li>
-    <li><a href="/login">Login</a></li>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+
+<ul class="row" id="bar">
+    <li class="col-lg-1"><a href="/"><img src="/images/books_logo.png"/></a></li>
+    <li class="col-lg-1" id="here_hov"><a href="/">Home</a></li>
+    <li class="col-lg-1" id="here_hov"><a href="/admin">Admin</a></li>
+    <li class="col-lg-offset-8 col-lg-1 glyphicon glyphicon-log-in" id="here_hov"><a href="/login">Login</a></li>
 </ul>
+</html>
