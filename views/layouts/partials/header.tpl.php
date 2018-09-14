b18b9bd6950a5da5288af0b7120893a1872132d8
Dia <dianapetrus13081997@gmail.com>
2018-09-14 06:01:01 -0700

    GBOOK-7: fixies after code review 2

--- views/layouts/partials/header.tpl.php
+++ views/layouts/partials/header.tpl.php
@@ -1,7 +1,3 @@
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
