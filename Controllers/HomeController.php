4df538de4fa10b53f04fedac0b232f9eab718b7a
Dia <dianapetrus13081997@gmail.com>
2018-09-17 03:36:51 -0700

    GBOOK8:Update homepage content

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -3,18 +3,22 @@
 namespace Controllers;
 
 
-class HomeController extends BasicController {
+class HomeController extends BasicController
+{
 
-  public function __construct() {
-    parent::__construct();
-    $this->title = "Homepage";
-  }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->title = "Homepage";
+	}
 
-  /**
-   * Callback for /home route.
-   */
-  public function homePageAction() {
-    $this->content = 'Homepage dummy content';
-    $this->renderLayout('/views/layouts/basic.tpl.php');
-  }
+	/**
+	 * Callback for /home route.
+	 */
+	public function homePageAction()
+	{
+		$this->content = $this->render('/views/home/home_content.tpl.php');
+		$sidebar = $this->render('/views/forms/home_search_form.tpl.php');
+		$this->renderLayout('/views/layouts/sidebar_page.tpl.php', array('sidebar' => $sidebar));
+	}
 }
