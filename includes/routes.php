22d5fca617df1faef1f977894e1cd1994ea02902
Laurentiu Lese <laurentiu.lese@freshbyteinc.com>
2018-09-20 14:34:13 +0300

    GBOOK-25: Add support for dynamic routing.

--- includes/routes.php
+++ includes/routes.php
@@ -27,6 +27,7 @@ function gbooks_routes_definitions()
       'GET' => 'Controllers\AdminController::adminConfigPageAction',
       'POST' => 'Controllers\AdminController::adminConfigPostAction',
     ),
+    '/book/{$id:([0-9]+)}' => 'Controllers\BookController::bookPageAction',
   );
 }
 
@@ -41,27 +42,55 @@ function gbooks_routes_execute_handler()
 
   $controllerClass = FALSE;
   $controllerAction = 'get';
+  $actionArguments = array();
+  $regex_routes = array();
 
-  if (isset($routes[$_GET['q']])) {
-    $routeDefinition = $routes[$_GET['q']];
-    if (is_array($routeDefinition) && isset($routeDefinition[$request_method])) {
-      $routeParts = explode('::', $routeDefinition[$request_method]);
-
-    } else {
-      $routeParts = explode('::', $routeDefinition);
-    }
-
-    if (!empty($routeParts)) {
-      $controllerClass = $routeParts[0];
-      if (isset($routeParts[1])) {
-        $controllerAction = $routeParts[1];
+  foreach ($routes as $route_path => $route_definition) {
+    $segments = explode('/', $route_path);
+    foreach ($segments as $index => &$segment) {
+      // We have a dynamic parameter.
+      if (strpos($segment, '{') !== FALSE) {
+        $segment = str_replace(array('{', '}'), array('', ''), $segment);
+        $segment_parts = explode(':', $segment);
+        $routes_arguments[$route_path][$segment_parts[0]] = $index;
+        $segment = $segment_parts[1];
       }
     }
+    $regex_path = implode('/', $segments);
+    $regex_path = str_replace('/', '\/', $regex_path);
+    $regex_routes[$regex_path]['definition'] = $route_definition;
+    $regex_routes[$regex_path]['original_route'] = $route_path;
+  }
+
+  foreach ($regex_routes as $regex => $routeInfo) {
+    $routeDefinition = $routeInfo['definition'];
+    if (preg_match('/^' . $regex . '$/', $_GET['q'])) {
+      if (is_array($routeDefinition) && isset($routeDefinition[$request_method])) {
+        $routeParts = explode('::', $routeDefinition[$request_method]);
+
+      } else {
+        $routeParts = explode('::', $routeDefinition);
+      }
+
+      if (!empty($routeParts)) {
+        $controllerClass = $routeParts[0];
+        if (isset($routeParts[1])) {
+          $controllerAction = $routeParts[1];
+        }
+      }
+      if (!empty($routes_arguments[$routeInfo['original_route']])) {
+        $path_segments = explode('/', $_GET['q']);
+        foreach ($routes_arguments[$routeInfo['original_route']] as $argument_name => $index) {
+          $actionArguments[$argument_name] = $path_segments[$index];
+        }
+      }
+    }
+
   }
 
   if ($controllerClass) {
     $controller = new $controllerClass();
-    $controller->{$controllerAction}();
+    call_user_func_array(array($controller, $controllerAction), $actionArguments);
   } else {
     echo '404 not found';
   }
