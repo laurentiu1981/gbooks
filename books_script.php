15259bcbae40d72464bbfea3037361c7542f0587
Dia <dianapetrus13081997@gmail.com>
2018-09-20 23:26:58 -0700

    GBOOK-23: Update import script to support 3 parameters instead of 2

--- books_script.php
+++ books_script.php
@@ -6,26 +6,19 @@ use scripts\BooksAPI;
 define('SITE_ROOT', getcwd());
 include_once(SITE_ROOT . '/includes/bootstrap.php');
 
-// check if there is at least 1 argument
-if ($argc === 1) {
-  trigger_error('you have to give at least 1 argument');
-  exit();
-} else if ($argc > 3) {
-  trigger_error('you can give at most 2 arguments');
+if ($argc !== 4) {
+  trigger_error('you have to give 3 arguments');
   exit();
 }
-//set category
-if ($argv[1] !== '') {
-  $category = $argv[1];
-} else {
-  trigger_error('you have to give at least 1 argument');
+
+$title = $argv[1];
+$category = $argv[2];
+$author = $argv[3];
+
+if ($argv[1] === '' && $argv[2] === '' && $argv[3] === '') {
+  trigger_error('you have to give at least 1 not empty argument');
+  exit();
 }
-//set author
-if (isset($argv[2])) {
-  $author = $argv[2];
-} else
-  $author = "";
 
-
-$booksAPI = new BooksAPI($author, $category);
+$booksAPI = new BooksAPI($title, $author, $category);
 $booksAPI->saveBooksToDatabase();
