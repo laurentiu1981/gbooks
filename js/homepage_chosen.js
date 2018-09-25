e5e1963c90029d3373ce7a52332918784560eb98
saboanca <sara_boanca@yahoo.com>
2018-09-25 12:02:14 +0300

    GBOOK-29: Add categories as filter for admin/home search forms. Fixes after code review.

--- js/homepage_chosen.js
+++ js/homepage_chosen.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     $(".chosen-select").prepend("<option value='' selected='selected'></option>");
-    $(".chosen-select").chosen({
-
-    });
+    $(".chosen-select").chosen({});
 });
 
