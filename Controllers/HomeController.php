1b1d5c2e5ef374ff3c54665596872db73c0a581d
saboanca <sara_boanca@yahoo.com>
2018-09-26 15:00:33 +0300

    GBOOK-19: Add functionality for homepage searchform. Fixes after code review.

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -22,35 +22,34 @@ class HomeController extends BasicController
   {
     $termModel = new TermModel();
     $bookModel = new BookModel();
+    $books = array();
     if ($_GET['q'] === '/search') {
       $services = new Services();
       if (empty($services->validationMessages($_GET))) {
-        set_search_fields(array('title' => $_GET['title'],
-          'author' => $_GET['author'],
-          'category' => $_GET['category'],
-          'priceFrom' => $_GET['price-from'],
-          'priceTo' => $_GET['price-to'],
-        ));
-        $searchBooks = $bookModel->generalFindBy($_GET['title'], $_GET['price-from'], $_GET['price-to'], $_GET['author'], $_GET['category']);
-        $homepageBooks = gbooks_generate_books($searchBooks);
-        $authorsOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("authors"), get_search_fields()['author']);
-        $categoriesOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("categories"), get_search_fields()['category']);
+        $books = $bookModel->generalFindBy($_GET['title'], $_GET['price-from'], $_GET['price-to'], $_GET['author'], $_GET['category']);
       } else {
         set_error_messages($services->validationMessages($_GET));
-        $authorsOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("authors"));
-        $categoriesOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("categories"));
-        $homepageBooks = '';
       }
     } else {
-      $authorsOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("authors"));
-      $categoriesOptions = gbooks_theme_generate_select_options($termModel->getTermNamesByVocabulary("categories"));
       $books = $bookModel->generalFindBy('', '', '', '', '', 12);
-      $homepageBooks = gbooks_generate_books($books);
     }
-    $searchFields = get_search_fields();
-    unset_search_fields($searchFields);
+
+    $homepageBooks = gbooks_generate_books($books);
+    $authorsOptions = gbooks_theme_generate_select_options(
+      $termModel->getTermNamesByVocabulary("authors"),
+      $_GET['author']
+    );
+    $categoriesOptions = gbooks_theme_generate_select_options(
+      $termModel->getTermNamesByVocabulary("categories"),
+      $_GET['category']
+    );
     $this->content = $this->render('/views/home/home_content.tpl.php', array('homepageBooks' => $homepageBooks));
-    $sidebar = $this->render('/views/forms/home_search_form.tpl.php', array('searchFields' => $searchFields, 'options' => $authorsOptions, 'optionsCategories' => $categoriesOptions, 'messages' => render_messages(get_messages())));
+    $sidebar = $this->render('/views/forms/home_search_form.tpl.php', array(
+      'searchFields' => $_GET,
+      'options' => $authorsOptions,
+      'optionsCategories' => $categoriesOptions,
+      'messages' => render_messages(get_messages())
+    ));
     $this->renderLayout('/views/layouts/sidebar_page.tpl.php', array('sidebar' => $sidebar));
   }
 }
