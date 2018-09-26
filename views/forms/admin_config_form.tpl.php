b5a0fc332ba708954aa2a8dee82edbf7c438ab82
Betty <melaniabetty95@gmail.com>
2018-09-26 15:21:47 +0300

    GBOOK-36: Add settings page to main menu.

--- views/forms/admin_config_form.tpl.php
+++ views/forms/admin_config_form.tpl.php
@@ -1,20 +1,14 @@
 <div class="messages">
   <?php echo $message; ?>
 </div>
-<form method="post" id="admin-config-form" class="text-center" action="">
-  <div class="row">
-    <div class="col-lg-4"></div>
-    <div class="form-group col-lg-4">
-      <label for="google_api">Google Api Endpoint</label>
-      <input type="text" class="form-control" id="google_api" name="google_api" value="<?php echo $gAPI; ?>">
-    </div>
+<form method="post" id="admin-config-form" action="">
+  <div class="form-group">
+    <label for="google_api">Google Api Endpoint</label>
+    <input type="text" class="form-control" id="google_api" name="google_api" value="<?php echo $gAPI; ?>">
   </div>
-  <div class="row">
-    <div class="col-lg-4"></div>
-    <div class="form-group col-lg-4">
-      <label for="max_books">Default Max books results per page</label>
-      <input type="number" class="form-control" id="max_books" name="max_books" min="0" value="<?php echo $maxBook; ?>">
-    </div>
+  <div class="form-group">
+    <label for="max_books">Default Max books results per page</label>
+    <input type="number" class="form-control" id="max_books" name="max_books" min="0" value="<?php echo $maxBook; ?>">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
